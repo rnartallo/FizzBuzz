f11cbc67941a72dcc365986d75646f491fbfee10
Ramon Nartallo <ramon.nartallo@softwire.com>
2021-07-06 10:10:05 +0100

    fizzbuzz

--- index.ts
+++ index.ts
@@ -1,1 +1,19 @@
 console.log('Hello world!')
+for (let i=1;i<101;i++){
+    var fb = false
+    if (i%15==0){
+        fb= true
+    }
+    if (i%3==0 && !fb){
+        console.log('Fizz')
+    }
+    else if(i%5 ==0 && !fb){
+        console.log('Buzz')
+    }
+    else if(fb){
+        console.log('Fizzbuzz')
+    }
+    else{
+        console.log(i)
+    }
+}
