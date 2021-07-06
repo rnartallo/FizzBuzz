3c29fe3a0a423de7dd320892596ac076da405e95
Ramon Nartallo <ramon.nartallo@softwire.com>
2021-07-06 10:35:59 +0100

    rules up to 13

--- index.ts
+++ index.ts
@@ -1,19 +1,53 @@
 console.log('Hello world!')
 for (let i=1;i<101;i++){
-    var fb = false
-    if (i%15==0){
-        fb= true
+    var div = false
+    var message =''
+    var hasfizz = false
+    var hasbuzz = false
+    var hasbang = false
+    var hasbong = false
+    var hasfezz = false
+    if (i%3==0){
+        div=true
+        hasfizz = true
     }
-    if (i%3==0 && !fb){
-        console.log('Fizz')
+    if(i%5 ==0){
+        hasbuzz= true
+        div = true
     }
-    else if(i%5 ==0 && !fb){
-        console.log('Buzz')
+    if(i%7==0){
+        div = true
+        hasbang = true
     }
-    else if(fb){
-        console.log('Fizzbuzz')
+    if(i%11==0){
+        div =true
+        hasbong = true
+    }
+    if(i%13==0){
+        div = true
+        hasfezz = true
+    }
+    if(div){
+        if (hasfizz && !hasbong){
+            message = message + "Fizz"
+        } 
+        if (hasfezz){
+            message = message + "Fezz"
+        }
+        if (hasbuzz && !hasbong){
+            message = message +"Buzz"
+        }
+        if (hasbang && !hasbong){
+            message = message +"Bang"
+        }
+        if (hasbong){
+            message = message + "Bong"
+        }
+
+        console.log(message)
     }
     else{
         console.log(i)
     }
+    
 }
